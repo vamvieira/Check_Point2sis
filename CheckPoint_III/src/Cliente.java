4936ab2ce5a3fee49c45647b6bb037c21c4aaccf
Vitor Magalhães Vieira <71799009+vamvieira@users.noreply.github.com>
2022-06-17 18:04:00 -0300

    Add files via upload

---
+++ CheckPoint_III/src/Cliente.java
@@ -0,0 +1,27 @@
+public class Cliente {
+    String nome, cliente, cpf, cnpj;
+        private int cpf;
+        private String nome;
+
+
+        public Cliente(String int cpf, nome, cliente, cnpj);
+        {
+                this.cpf = cpf;
+                this.nome = nome;
+        }
+        public String getNome ()
+        {
+                return nome;
+        }
+        public void setNome (String novoNome){}
+        public int getCpf ()
+        {
+                return cpf;
+        }
+        public void setCpf (int novoCpf){}
+        public int getCnpj ()
+        {
+                return cnpj;
+        }
+        public void setCnpj (int novoCnpj){}
+}
